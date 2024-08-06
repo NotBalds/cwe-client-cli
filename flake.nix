e603eef8fcf1dd31e0907ba489b58009349c724c
ProggerX <x@proggers.ru>
2024-08-06 09:53:09 +0300

    Added flake package

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
 					gtk3
 					cairo
 					gdk-pixbuf
+					pkg-config
 					glib
 					dbus
 					openssl_3
@@ -43,6 +44,14 @@
 							export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
 							export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
 						'';
+				};
+				packages.default = pkgs.rustPlatform.buildRustPackage rec {
+					pname = "cwe-client-cli";
+					version = "0.1.3";
+					buildInputs = libraries ++ packages;
+					nativeBuildInputs = libraries ++ packages;
+					src = ./.;
+					cargoSha256 = "sha256-hje3y8IDFgrHTifsDPYqodYIB4Bqqj490ChS9VhSTk4=";
 				};
 			});
 }
