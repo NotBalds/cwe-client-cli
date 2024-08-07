9405fe9ef1030915877a36b6ac03e74910b7a512
icelayer <ice1ay3r@gmail.com>
2024-08-07 12:01:00 +0300

    Added list of supported types

--- src/modules/config.rs
+++ src/modules/config.rs
@@ -4,6 +4,8 @@ use std::path::PathBuf;
 
 pub const VERSION: &str = "0.2.0";
 
+pub const SUPPORTED_TYPES: [&str; 2] = ["Text", "Image"];
+
 pub fn path() -> PathBuf {
     ProjectDirs::from("su", "bald", "cwe-client")
         .unwrap()
@@ -13,6 +15,10 @@ pub fn path() -> PathBuf {
 
 pub fn default_url() -> String {
     String::from("http://bald.su:1337/")
+}
+
+pub fn supported_types() -> Vec<String> {
+    SUPPORTED_TYPES.iter().map(|x| x.to_string()).collect()
 }
 
 pub fn url(path: &str) -> String {
