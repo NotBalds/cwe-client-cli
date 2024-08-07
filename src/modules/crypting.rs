4ac6f2d5861e1e1a7ba6a635532d80722f906202
icelayer <ice1ay3r@gmail.com>
2024-08-07 13:00:04 +0300

    Testing new type of encrypting

--- src/modules/crypting.rs
+++ src/modules/crypting.rs
@@ -7,14 +7,14 @@ use {
         pkey::{PKey, Public},
         rsa::{Padding, Rsa},
         sign::Signer,
-        symm::Cipher,
+        symm::{self, Cipher},
     },
+    rand::{rngs::OsRng, RngCore},
+    std::io,
 };
 
-const BITS: u32 = 2048;
-
-pub fn gen_keys(passphrase: String) -> (String, String) {
-    let rsa = Rsa::generate(BITS).unwrap();
+pub fn gen_keys(passphrase: String, bits: u32) -> (String, String) {
+    let rsa = Rsa::generate(bits).unwrap();
 
     let private_key: Vec<u8> = rsa
         .private_key_to_pem_passphrase(Cipher::aes_256_cbc(), passphrase.as_bytes())
@@ -78,6 +78,56 @@ pub fn encrypt(data: Vec<u8>, public_key: String) -> String {
     b64.encode(encrypted_data)
 }
 
+fn encrypt_data(public_key: String, input_data: &[u8]) -> String {
+    let rsa = match Rsa::public_key_from_pem_pkcs1(public_key.as_bytes()) {
+        Ok(object) => object,
+        Err(err) => {
+            base::log(
+                &format!("Can't convert public key from pem to object: {}", err),
+                1,
+            );
+            let tmp: Rsa<Public> = Rsa::public_key_from_pem(public_key.as_bytes()).unwrap();
+            tmp
+        }
+    };
+
+    let mut sym_key = vec![0; 32];
+    let mut iv = vec![0; 16];
+
+    OsRng.fill_bytes(&mut sym_key);
+    OsRng.fill_bytes(&mut iv);
+
+    let mut encrypted_key = vec![0; rsa.size() as usize];
+    let encrypted_key_len = match rsa.public_encrypt(&sym_key, &mut encrypted_key, Padding::PKCS1) {
+        Ok(size) => size,
+        Err(err) => {
+            base::log(&format!("Can't encrypt symmetric key: {}", err), 1);
+            0
+        }
+    };
+
+    let mut output = Vec::new();
+
+    output.extend_from_slice(&encrypted_key[..encrypted_key_len]);
+    output.extend_from_slice(&iv);
+
+    let cipher = Cipher::aes_256_cbc();
+    let encrypted_data = match symm::encrypt(cipher, &sym_key, Some(&iv), input_data) {
+        Ok(data) => data,
+        Err(err) => {
+            base::log(&format!("Can't encrypt data: {}", err), 1);
+            vec![]
+        }
+    };
+
+    output.extend_from_slice(&encrypted_data);
+
+    sym_key.fill(0);
+    iv.fill(0);
+
+    b64.encode(output)
+}
+
 pub fn decrypt(data: String, passphrase: String) -> String {
     let rsa = Rsa::private_key_from_pem_passphrase(
         filesystem::cat(&filesystem::new_path("base-keys/my-key")).as_bytes(),
@@ -114,6 +164,56 @@ pub fn decrypt(data: String, passphrase: String) -> String {
             String::new()
         }
     }
+}
+
+fn decrypt_data(private_key: String, encrypted_data: &str) -> Vec<u8> {
+    let rsa = match Rsa::private_key_from_pem(private_key.as_bytes()) {
+        Ok(object) => object,
+        Err(err) => {
+            base::log(
+                &format!("Can't convert private key from pem to object: {}", err),
+                1,
+            );
+            return vec![];
+        }
+    };
+
+    let decoded_data = b64.decode(encrypted_data).unwrap();
+    let rsa_size = rsa.size() as usize;
+    let iv_len = 16;
+
+    if decoded_data.len() < rsa_size + iv_len {
+        base::log("Data corrypted. Can't decrypt data", 1);
+        return vec![];
+    }
+
+    let encrypted_key = &decoded_data[..rsa_size];
+    let iv = &decoded_data[rsa_size..rsa_size + iv_len];
+    let encrypted_content = &decoded_data[rsa_size + iv_len..];
+
+    let sym_key = {
+        let mut key = vec![0; rsa_size];
+        let decrypted_size = match rsa.private_decrypt(encrypted_key, &mut key, Padding::PKCS1) {
+            Ok(size) => size,
+            Err(err) => {
+                base::log(&format!("Can't decrypt symmetric key: {}", err), 1);
+                return vec![];
+            }
+        };
+        key.truncate(decrypted_size);
+        key
+    };
+
+    let cipher = Cipher::aes_256_cbc();
+    let decrypted_data = match symm::decrypt(cipher, &sym_key, Some(iv), encrypted_content) {
+        Ok(data) => data,
+        Err(err) => {
+            base::log(&format!("Can't decrypt data: {}", err), 1);
+            return vec![];
+        }
+    };
+
+    decrypted_data
 }
 
 pub fn correct_passphrase(passphrase: &str) -> bool {
