b04472afbc3116d6bd7fd2c59e1c6c45b1fb4a0d
TBWanderer <evgenypodyryaka@yandex.ru>
2025-02-14 19:36:04 +0300

    Added run func

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,2 +1,15 @@
 pub mod cfg;
+pub mod cmd;
 pub mod init;
+
+use notblib::stat;
+
+pub fn run() {
+    match init::run() {
+        Err(err) => stat::fatal(err.to_string()),
+        _ => match cmd::run() {
+            Err(err) => stat::fatal(err.to_string()),
+            _ => return,
+        },
+    };
+}
